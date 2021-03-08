6ed2a851149923add456e1954842ea1258db1925
matheusazambuja <matheusazambuja35@gmail.com>
2021-03-08 14:19:49 -0300

    Add SideNavBar in page Leaderboard

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -2,14 +2,24 @@ import Head from 'next/head'
 import React from 'react'
 import SideNavBar from '../components/SideNavBar'
 
+import { Box, Text } from '@chakra-ui/layout'
+
 export default function Leaderboard() {
   return (
     <div>
       <Head>
         <title>Leaderboard | move.it</title>
       </Head>
-      <SideNavBar />
-      <h1>Leaderboard</h1>
+      <Box as="div" display="flex" height="100vh">
+        <SideNavBar />
+        <Text as="h1"
+          display="flex"
+          alignItems="center"
+          textAlign="center"
+        >
+          Leaderboard
+        </Text>
+      </Box>
     </div>
   )
 }
